aec680649453c5baad11e79f8efcb1c71a9c2d99
Escaduma <MEscaleraDuron@csi.edu>
2026-08-24 11:19:21 -0600

    Create tables and insert initial animal data

---
+++ week2/week2lab.sql
@@ -0,0 +1,32 @@
+--Query 1
+CREATE TABLE animal_types(
+animal_id numeric,
+species varchar(50),
+habitat varchar(50),
+diet varchar(100)
+);
+
+CREATE TABLE animals(
+an_id numeric,
+animal_name varchar(50),
+species_id numeric,
+age numeric
+);
+
+--Query 2
+INSERT INTO animal_types(animal_id, species, habitat, diet)
+VALUES 
+(1, 'Lion', 'Grassland', 'Meat'),
+(2, 'Giraffe', 'Savanna','Leaves, shoots, flowers, and fruits'),
+(3, 'Zebra', 'Grasslands and Savannas', 'Plants');
+
+select * from animal_types;
+
+--Query 3
+INSERT INTO animals(an_id, animal_name, species_id, age)
+VALUES 
+(15, 'Carola', 1, 10),
+(60, 'Satine', 2, 5),
+(25, 'Rufino', 3, 8);
+
+select * from animals;
