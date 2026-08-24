b08bb0c2a459d252d33b9e967a5ba8a442d20ae0
Escaduma <MEscaleraDuron@csi.edu>
2026-08-24 11:33:33 -0600

    Enhance week2practice.sql with comments
    Added comments to SQL queries for better understanding.

--- week2/week2practice.sql
+++ week2/week2practice.sql
@@ -1,5 +1,7 @@
+--QUERY 1
+CREATE DATABASE week2lab --Create a new database
 --Query 1
-CREATE TABLE teachers(
+CREATE TABLE teachers( -- Used to create a new table 
 teacher_id bigserial PRIMARY KEY, 
 first_name varchar(25), 
 last_name varchar(50), 
@@ -7,9 +9,9 @@ hire_date date,
 salary numeric);
 
 --Query 2
-select * from teachers;
+select * from teachers; --It shows the information in the table
 
 --Query 3
-INSERT INTO teachers(first_name, last_name, hire_date, salary)
+INSERT INTO teachers(first_name, last_name, hire_date, salary) --Insert information (data) into te table created
 VALUES ('Alex', 'Alavez', '2025-08-01', 00000), 
 ('Maria', 'Vega', '2025-08-01', 000);
